74778afde7eb92f52dd05c3060307f70ddc134dc
Wes <wesdowney@gmail.com>
2017-01-29 14:10:39 -0500

    More descriptive errors

--- front-end-development-certification/Intermediate-Front-End-Development-Projects/Build-A-Random-Quote-Machine/js/index.js
+++ front-end-development-certification/Intermediate-Front-End-Development-Projects/Build-A-Random-Quote-Machine/js/index.js
@@ -1,21 +1,18 @@
 function getQuote() {
 
 // Get a quote from Famous Quotes API on MashApe
-var quote = $.ajax({
+$.ajax({
     url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
     type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
     data: {}, // Additional parameters here
     dataType: 'json',
     success: function(data) {
-    	//
-        //Change data.source to data.something , where something is whichever part of the object you want returned.
-        //To see the whole object you can output it to your browser console using:
-        //console.log(data);
        	document.getElementById("quote-text").innerHTML = data.quote; 
        	document.getElementById("quote-author").innerHTML = data.author; 
-       	//alert(Object.keys(data));
-        },
-    error: function(err) { alert(err); },
+    },
+    error: function(err) { 
+    	alert("Error: " + err.status + ". " + err.responseText);	
+    },
     beforeSend: function(xhr) {
     xhr.setRequestHeader("X-Mashape-Authorization", "e1wdzgHAD2mshnZlaKpZkmJF33lNp1AonJFjsnfA0zY6PiYl3E"); // Enter here your Mashape key
     }
