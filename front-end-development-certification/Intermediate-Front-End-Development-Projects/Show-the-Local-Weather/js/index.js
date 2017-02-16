880fe33429bae2d5c3afa59ef041e4a04334901b
Wes <wesdowney@gmail.com>
2017-02-16 11:09:45 -0500

    Added OpenWeatherMap API

--- front-end-development-certification/Intermediate-Front-End-Development-Projects/Show-the-Local-Weather/js/index.js
+++ front-end-development-certification/Intermediate-Front-End-Development-Projects/Show-the-Local-Weather/js/index.js
@@ -6,6 +6,10 @@ $( document ).ready(function() {
 
   		// Split response's latitude and longitude into an array.
   		location.loc = location.loc.split(',');
+
+  		$.get("//api.openweathermap.org/data/2.5/weather?lat=" + location.loc[0] + "&lon=" + location.loc[1] + "&APPID=326b00a90b7281cb02b6f79e3284673c", function(response) {
+			alert(JSON.stringify(response, null, 4));
+		}, "jsonp")
 	}, "jsonp")
 
 });
