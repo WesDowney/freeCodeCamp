db066ba3213940ab1475f844d6bded6098d6e399
Wes <wesdowney@gmail.com>
2017-02-20 19:12:30 -0500

    Background changes depending on weather condition

--- front-end-development-certification/Intermediate-Front-End-Development-Projects/Show-the-Local-Weather/js/index.js
+++ front-end-development-certification/Intermediate-Front-End-Development-Projects/Show-the-Local-Weather/js/index.js
@@ -13,12 +13,33 @@ $( document ).ready(function() {
 
   		// Query the OpenWeatherMap API to return weather data based on the user's location
   		$.get("//api.openweathermap.org/data/2.5/weather?lat=" + location.loc[0] + "&lon=" + location.loc[1] + "&APPID=326b00a90b7281cb02b6f79e3284673c", function(response) {
+			// alert(JSON.stringify(response, null, 4)); // Shows the whole returned object nicely formatted for debugging 
+
 			// Convert the returned temperature from Kelvin to Fahrenheit and display it. 
 			Fahrenheit = Math.round((response.main.temp * (9/5)) - 459.67);
 			Celsius = Math.round((Fahrenheit - 32) * (5/9));
 			displayFahrenheit();
 			document.getElementById("condition").innerHTML = toTitleCase(response.weather[0].description);
-			// alert(JSON.stringify(response, null, 4)); // Shows the whole returned object nicely formatted for debugging 
+
+			// Set the background image depending on weather condition. http://openweathermap.org/weather-conditions
+			// All images free to use personal/commercial from pexels.com and pixabay.com
+			switch(response.weather[0].main) {
+				case "Clear": 
+					$('body').css('background', 'url(img/clear.jpg) no-repeat center center fixed'); 
+					break;
+				case "Clouds":
+					$('body').css('background', 'url(img/clouds.jpeg) no-repeat center center fixed');
+					break;
+				case "Rain":
+					$('body').css('background', 'url(img/rain.jpeg) no-repeat center center fixed');
+					break;
+				case "Thunderstorm":
+					$('body').css('background', 'url(img/lightning.jpg) no-repeat center center fixed');
+					break;
+				case "Snow":
+					$('body').css('background', 'url(img/snow.jpg) no-repeat center center fixed');
+					break;
+			}		
 		}, "jsonp")
 	}, "jsonp")
 
