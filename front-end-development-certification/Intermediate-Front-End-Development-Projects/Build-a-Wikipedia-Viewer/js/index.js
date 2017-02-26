63824d90b7cabafa0394a09e0de006639ae343e6
Wes <wesdowney@gmail.com>
2017-02-26 14:51:42 -0500

    Veritical align search box after button clicked and Cleanup

--- front-end-development-certification/Intermediate-Front-End-Development-Projects/Build-a-Wikipedia-Viewer/js/index.js
+++ front-end-development-certification/Intermediate-Front-End-Development-Projects/Build-a-Wikipedia-Viewer/js/index.js
@@ -10,9 +10,19 @@ function searchWikipedia() {
 	    url: remoteUrlWithOrigin,
 	    dataType: 'jsonp', // jsonp helps with cross origin error
 	    type: 'GET',
-	    headers: { 'FCC Wikipedia Viewer App': 'v.1' },
+	    headers: { 'FCC Wikipedia Viewer App': 'v.1' }, // Wikipedia API requires custom user agent
 	    success: function(data) {
-	       alert(data);
+	    	// alert(JSON.stringify(data, null, 4)); // Shows the whole returned object nicely formatted for debugging 
+	    	/* 
+		    	data[1] are titles
+		    	data[2] are descriptions
+		    	data[3] are links
+		    */ 
+	    	alert(data[3][0]);
+
+	    	// Change the search box to align to vertically align top
+	    	var container = document.getElementById("container");
+			container.className += " vert-align-top";
 	    }
 	} );
 
