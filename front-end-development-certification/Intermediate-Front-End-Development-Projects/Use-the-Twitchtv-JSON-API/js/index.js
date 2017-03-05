093728ef7e325d5658dd3bca2c3a50d747a1cc08
Wes <wesdowney@gmail.com>
2017-03-05 13:27:20 -0500

    Twitch App: Set up Bootstrap Cards to display streamers

--- front-end-development-certification/Intermediate-Front-End-Development-Projects/Use-the-Twitchtv-JSON-API/js/index.js
+++ front-end-development-certification/Intermediate-Front-End-Development-Projects/Use-the-Twitchtv-JSON-API/js/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
-	var channel = 'lirik';
-	var twitchPassThroughAPI = "https://wind-bow.gomix.me/twitch-api/channels/" + channel;
+	/* Active Channels: lirik */
+	var usernames = ["lirik", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+	var twitchPassThroughAPI = "https://wind-bow.gomix.me/twitch-api/streams/" + usernames[0];
 
 	$.ajax( {
 	    url: twitchPassThroughAPI,
@@ -14,20 +15,24 @@ $(document).ready(function(){
 				Sample Data I'll be using: 
 
 				From '/streams/:stream'
-					
-				
+				 	"stream": null 
+				 	"viewers": 35786,
+				 	"created_at": "2017-03-04T17:06:57Z" - Maybe use for time streaming
+				 	"preview": { 
+				 		"medium": "https://static-cdn.jtvnw.net/previews-ttv/live_user_lirik-320x180.jpg",
+				 		"template": "https://static-cdn.jtvnw.net/previews-ttv/live_user_lirik-{width}x{height}.jpg"
+
 				From '/channels/:channel'
-					"logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/hebo-profile_image-6947308654ad603f-300x300.jpeg"
-					"url": "https://www.twitch.tv/hebo",
-					"status": "Zelda: Breath of The Weeb - Twitter: @LIRIK "
 					"display_name": "LIRIK"
+					"logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/hebo-profile_image-6947308654ad603f-300x300.jpeg"
+					"status": "Zelda: Breath of The Weeb - Twitter: @LIRIK ",
+					"game": "The Legend of Zelda: Breath of the Wild"
+					"url": "https://www.twitch.tv/lirik",
 					"views": 162090817,
 	    			"followers": 1587337,
-
 			*/
 
-	    	alert(JSON.stringify(data, null, 4)); // Shows the whole returned object nicely formatted for debugging 
-	    	// alert(data);
+	    	// alert(JSON.stringify(data, null, 4)); // Shows the whole returned object nicely formatted for debugging 
 	    }
 	} );
 
