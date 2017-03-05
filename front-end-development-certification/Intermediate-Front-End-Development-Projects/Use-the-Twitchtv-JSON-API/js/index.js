b2459cf2c8f0e368d8eb1bfa09bd4f65cd7aaa31
Wes <wesdowney@gmail.com>
2017-03-04 20:58:48 -0500

    Twitch App - Added Bootstrap 4

--- front-end-development-certification/Intermediate-Front-End-Development-Projects/Use-the-Twitchtv-JSON-API/js/index.js
+++ front-end-development-certification/Intermediate-Front-End-Development-Projects/Use-the-Twitchtv-JSON-API/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	var channel = 'hebo';
+	var channel = 'lirik';
 	var twitchPassThroughAPI = "https://wind-bow.gomix.me/twitch-api/channels/" + channel;
 
 	$.ajax( {
@@ -9,6 +9,19 @@ $(document).ready(function(){
 	    type: 'GET',
 	    headers: { 'Twitch API App': 'v.1' }, 
 	    success: function(data) {
+
+			/*
+				Sample Data I'll be using: 
+
+				"logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/hebo-profile_image-6947308654ad603f-300x300.jpeg"
+				"url": "https://www.twitch.tv/hebo",
+				"status": "Zelda: Breath of The Weeb - Twitter: @LIRIK "
+				"display_name": "LIRIK"
+				"views": 162090817,
+    			"followers": 1587337,
+
+			*/
+
 	    	alert(JSON.stringify(data, null, 4)); // Shows the whole returned object nicely formatted for debugging 
 	    	// alert(data);
 	    }
