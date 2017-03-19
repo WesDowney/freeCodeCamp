8cad5ceac490202ddb5dab0511bf9afefbda13d5
Wes <wesdowney@gmail.com>
2017-03-18 22:09:38 -0400

    Twitch App - If API returns 404, the app displays messaging accordingly

--- front-end-development-certification/Intermediate-Front-End-Development-Projects/Use-the-Twitchtv-JSON-API/js/index.js
+++ front-end-development-certification/Intermediate-Front-End-Development-Projects/Use-the-Twitchtv-JSON-API/js/index.js
@@ -1,4 +1,4 @@
-var streamers = ["dreadztv", "lirik", "freecodecamp", "LCK1", "ESL_SC2", "olofmeister"];
+var streamers = ["dreadztv", "lirik", "freecodecamp", "LCK1", "ESL_SC2", "olofmeister", "brunofin", "comster404", "curse"];
 
 function populateStreamerCards (i) {
 	var twitchPassThroughAPI = "https://wind-bow.gomix.me/twitch-api/channels/";
@@ -13,16 +13,30 @@ function populateStreamerCards (i) {
 	    success: function(data) {
 	    	// Populate cards with the channel data
 			var card = document.createElement('div');
-			card.className = 'card';
-			card.id = 'card-' + data.display_name;
 
-			// Populate the card with streamer data
-			card.innerHTML = '<img class="card-img-top" src="' + data.logo + '" alt="Stream Image">\
-							  <div class="card-block">\
-							  	<h4 class="card-title">' + data.display_name + '</h4>\
-								<p class="card-text">' + data.status + '</p>\
-								<a href="' + data.url +'" class="btn btn-primary" id="button-' + data.display_name + '">View Stream</a>\
-							  </div>'; 
+			// Check to see if it returned a 404 error
+			if (data.status == 404) {
+				// The user either closed their account or it never existed
+				card.className = 'card offline';
+				card.id = 'card-404';
+
+				card.innerHTML = '<img class="card-img-top" src="img/Glitch_Purple.png" alt="Channel 404">\
+								  <div class="card-block">\
+									<p class="card-text">' + data.message + ' or has been closed</p>\
+									<a href="#" class="btn btn-primary disabled" id="button-404">Channel Error</a>\
+								  </div>'; 
+	    	} else {
+				// API Returned Streamer data correctly. Populate the card with streamer data
+
+				card.className = 'card';
+				card.id = 'card-' + data.display_name;
+				card.innerHTML = '<img class="card-img-top" src="' + data.logo + '" alt="Stream Image">\
+								  <div class="card-block">\
+								  	<h4 class="card-title">' + data.display_name + '</h4>\
+									<p class="card-text">' + data.status + '</p>\
+									<a href="' + data.url +'" class="btn btn-primary" id="button-' + data.display_name + '">View Stream</a>\
+								  </div>'; 
+	    	}
 
 			// Append the card to the streamer section
 			streamerSection.appendChild(card);
@@ -40,8 +54,8 @@ function fadeOfflineCards(streamer) {
 	    type: 'GET',
 	    headers: { 'Twitch API App': 'v.1' }, 
 	    success: function(data) {
+	    	alert(JSON.stringify(data, null, 4)); // Shows the whole returned object nicely formatted for debugging
 	    	if (data.stream == null){ 
-	    		// alert(JSON.stringify(data, null, 4)); // Shows the whole returned object nicely formatted for debugging
 				// The streamer is offline. Add the offline class to fade out the card
 				$( '#card-' + streamer ).addClass( 'offline' );
 
